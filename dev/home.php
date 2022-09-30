8fb261ac14961a0b56f3405e42ff985a9a8aa9f2
jessicach4n <e.jchan@etu.cvm.qc.ca>
2022-09-30 13:51:50 -0400

    Home CSS base done

--- dev/home.php
+++ dev/home.php
@@ -7,9 +7,17 @@
 	require_once("partial/header.php");
 ?>
 
+<head>
+	<link rel="stylesheet" href="css/home.css">
+</head>
+
 <body class="home">
-	<h1>Générateur d'art météorlogique</h1>
-	<h2>L'expérience commence </h2>
-    <a href="animation.php">ici</a>
+	<div class="home-content">
+		<div class="headings-content">
+			<h1 class="nom-site">Générateur d'art météorlogique</h1>
+			<h1>L'expérience<br>commence</h1>
+		</div>
+		<a id="go-to-animation" href="animation.php">ici</a>
+	</div>
 </body>
 </html>

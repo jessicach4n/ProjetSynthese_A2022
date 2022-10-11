64b82712aa8a3ab69599b78f6d8ec6aa20e59d2f
jessicach4n <e.jchan@etu.cvm.qc.ca>
2022-10-11 15:26:00 -0400

    Login and register

---
+++ dev/action/MotDePasseOublie.php
@@ -0,0 +1,17 @@
+<?php
+	require_once("action/CommonAction.php");
+
+	class MotDePasseOublie extends CommonAction {
+
+		public function __construct() {
+			parent::__construct(CommonAction::$VISIBILITY_PUBLIC);
+		}
+
+		protected function executeAction() {
+			$pageName = "MotDePasseOublie";
+			$active = "active-page";
+
+			return compact("pageName", "active");
+		}
+
+	}
---
+++ dev/motdepasseoublie.php
@@ -0,0 +1,22 @@
+<?php
+	require_once("action/MotDePasseOublie.php");
+	
+	$action = new MotDePasseOublie();
+	$data = $action->execute();
+
+	require_once("partial/header.php");
+?>
+
+<head>
+	<link rel="stylesheet" href="css/login.css">
+</head>
+
+<body class="motdepasseoublie">
+</body>
+    <h1>Réinitialiser le mot de passe</h1>
+	<form action="" method="post">
+		<input type="email" name="input-field" placeholder="Email" id="email" >
+		<button type="submit">Envoyer</button>
+    </form>
+
+	</html>

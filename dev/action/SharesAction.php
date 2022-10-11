a1d6f018b2e34ed1866f92dafc08c9d2619bcb0d
Epsot <sebasperezdoria@hotmail.com>
2022-10-11 16:11:08 -0400

    missing is conencted Session variable

--- dev/action/SharesAction.php
+++ dev/action/SharesAction.php
@@ -4,12 +4,14 @@
 	class SharesAction extends CommonAction {
 
 		public function __construct() {
-			parent::__construct(CommonAction::$VISIBILITY_PUBLIC);
+			parent::__construct(CommonAction::$VISIBILITY_MEMBER);
 		}
 
 		protected function executeAction() {
 			$pageName = "Shares";
 			$active = "active-page";
+
+			var_dump($_SESSION["visibility"]);
 
 			return compact("pageName", "active");
 		}

e95adc4602e6886913471c57a8602f152723afc6
Epsot <sebasperezdoria@hotmail.com>
2022-11-23 17:45:28 -0500

    encapsulation 1/2

---
+++ dev/projet-synthese/src/pages/components/generativeAlgorithm/ProcessorBezier.js
@@ -0,0 +1,64 @@
+import { MyAwesomeQueue } from './MyAwesomeQueue';
+import { GenerativeAlgorithm } from './GenerativeAlgorithm';
+export class ProcessorBezier  extends GenerativeAlgorithm{
+  constructor() {
+    super()
+    this.members = [];
+    this.queue = new MyAwesomeQueue(4)
+    }
+
+    createMembers(memberNumber){
+      for(let i = 0 ; i < memberNumber ; i++){
+        let ball = {x : 0, y: 0, p2x : 0, p2y : 0} 
+        this.members.push(ball);
+      }
+      
+    }
+    getMembers(){
+      return this.members;
+    }
+    setMiddlePoint(){
+      for(let member of this.members){
+        member.p2x = Math.floor(Math.random() * this.width)
+        member.p2y = Math.floor(Math.random() * this.height)
+      }
+    }
+
+    setOrderMarginPoints(tab){
+      for (let element in tab){
+        this.queue.enqueue(element)
+      }
+    }
+
+    goToNextPoint(){
+      let direction = this.queue.getFront()
+      let point = this.setNexMargintPoint(direction)
+      this.queue.circulatePoints()
+      return point
+    }
+
+    setNexMargintPoint(side){
+      let point ={};
+    //*SIDES TOP:1 RIGHT:2 BOTTOM:3 LEFT:4       
+    if (side == 1){
+        point = {x: Math.floor(Math.random() * this.width), y: 0};
+      }
+      
+      if (side == 2){
+        point = {x: this.width, y: Math.floor(Math.random() * this.height)};
+      }
+      
+      if (side == 3){
+
+        point = {x: Math.floor(Math.random() * this.width) , y: this.height};
+      }
+      
+      if (side == 4){
+        point = {x: 0  , y: Math.floor(Math.random() * this.height)};
+      }
+      return point
+    }
+
+   
+
+  }
--- dev/projet-synthese/src/pages/components/generativeAlgorithm/TriangleBezierCollisionAlgorithm.js
+++ dev/projet-synthese/src/pages/components/generativeAlgorithm/TriangleBezierCollisionAlgorithm.js
@@ -3,6 +3,7 @@ import { MyAwesomeQueue } from './MyAwesomeQueue';
 import {ProcessorSin} from './ProcessorSin';
 import {ProcessorCos} from './ProcessorCos';
 import { Component } from "react";
+import { ProcessorBezier } from './ProcessorBezier';
 import Sketch from "react-p5";
 
 export default class TriangleBezierCollisionAlgorithm extends GenerativeAlgorithm {
@@ -17,42 +18,22 @@ export default class TriangleBezierCollisionAlgorithm extends GenerativeAlgorith
     // this.averageVisibility      
     // this.humidity    
     }
-    //*SIDES
-    //*TOP : 1
-    //*RIGHT : 2
-    //*BOTTOM : 3
-    //*LEFT : 4
-    genratePoint(side){
-        let point = {}
-        
-        if (side == 1){
-            point = {x: Math.floor(Math.random() * this.width), y: 0 + this.padding}
-        }
-        
-        if (side == 2){
-            point = {x: this.width, y: Math.floor(Math.random() * this.height)}
-        }
-        
-        if (side == 3){
-            point = {x: Math.floor(Math.random() * this.width) , y: this.height}
-        }
 
-        if (side == 4){
-            point = {x: 0 + this.padding , y: Math.floor(Math.random() * this.height)}
-        }
-        return point
-    }
     render() {
 
-            let width = window.innerWidth - 250
-            let height = window.innerHeight - 250
             let t = 0
             let begin = true
-            let ball   = {x : 0, y: 0, p2x : 150, p2y : 120} 
-            let ball2  = {x : 0, y: 0, p2x : 150, p2y : 120} 
-            let ball3  = {x : 0, y: 0, p2x : 150, p2y : 120} 
-            let balls = [ball,ball2, ball3]
-            
+            //*SIDES TOP:1 RIGHT:2 BOTTOM:3 LEFT:4 
+            let marginOrder = [1,2,3,4]
+
+            let processorBezier = new ProcessorBezier()
+            processorBezier.createMembers(3)
+            processorBezier.setMiddlePoint()
+            let balls = processorBezier.getMembers()
+            processorBezier.setOrderMarginPoints(marginOrder)    
+
+            processorBezier.goToNextPoint()
+                        
             
             let ballNextPointQueue = new MyAwesomeQueue(4)
             ballNextPointQueue.enqueue(1)
@@ -61,8 +42,11 @@ export default class TriangleBezierCollisionAlgorithm extends GenerativeAlgorith
             ballNextPointQueue.enqueue(4)
             
             let direction = ballNextPointQueue.getFront()
-            let newLastPoint = this.genratePoint(direction)
-            let newfirstPoint = this.genratePoint(4)
+           
+            let newLastPoint = processorBezier.setNexMargintPoint(direction)
+            let newfirstPoint = processorBezier.setNexMargintPoint(4)
+
+            // console.log("new last point " + newLastPoint.x)
             let p1 = {x :newfirstPoint.x, y : newfirstPoint.y}
             // let p2 = {x : Math.floor(Math.random() * width), y : Math.floor(Math.random() * height)}
             let p3 = {x : newLastPoint.x, y : newLastPoint.y}
@@ -81,37 +65,31 @@ export default class TriangleBezierCollisionAlgorithm extends GenerativeAlgorith
                 for(let member of balls){
                 p5.fill('teal')
                 p5.ellipse(member.x, member.y , 25, 25)
+
+                p5.fill('orange')
+                p5.ellipse(member.p2x, member.p2y , 25, 25) 
+
+                member.x = Math.pow((1-t),2) * p1.x + 2 * (1-t) * t * member.p2x + Math.pow(t,2) * p3.x
+                member.y = Math.pow((1-t),2) * p1.y + 2 * (1-t) * t * member.p2y + Math.pow(t,2) * p3.y
             }
-                
                 p5.fill('pink')
                 p5.ellipse(p1.x, p1.y , 25, 25)
-                
-                for(let member of balls){
-                    
-                    p5.fill('orange')
-                    p5.ellipse(member.p2x, member.p2y , 25, 25) 
-                } 
                 
                 p5.fill('yellow')
                 p5.ellipse(p3.x, p3.y , 25, 25) 
                 
-                for (let member of balls){
-                    member.x = Math.pow((1-t),2) * p1.x + 2 * (1-t) * t * member.p2x + Math.pow(t,2) * p3.x
-                    member.y = Math.pow((1-t),2) * p1.y + 2 * (1-t) * t * member.p2y + Math.pow(t,2) * p3.y
-                }
-
                 if (t < 1 && begin){
                     t+=0.01   
                 }
                 else{
                     direction = ballNextPointQueue.getFront()
                     ballNextPointQueue.circulatePoints()
-                    let newLastPoint = this.genratePoint(direction)
+                    // let newLastPoint = processorBezier.goToNextPoint
+                    let newLastPoint = processorBezier.setNexMargintPoint(direction)
                     p1 = p3
-                    // p2 = {x : Math.floor(Math.random() * width), y : Math.floor(Math.random() * height)}
                     for (let member of balls){
-                        member.p2x = Math.floor(Math.random() * width)
-                        member.p2y = Math.floor(Math.random() * height)
+                        member.p2x = Math.floor(Math.random() * this.width)
+                        member.p2y = Math.floor(Math.random() * this.height)
 
                     }
                     p3 = {x : newLastPoint.x, y : newLastPoint.y}

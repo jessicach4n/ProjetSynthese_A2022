a2037c2f695c17b01b34e0da6e70a81bc63c32f9
jessicach4n <e.jchan@etu.cvm.qc.ca>
2022-12-02 18:35:54 -0500

    constant for is logged in

--- dev/projet-synthese/src/pages/constants.js
+++ dev/projet-synthese/src/pages/constants.js
@@ -4,3 +4,4 @@ export const HOST = 'http://localhost:8888';
 export const GET_IDS = "GET_IDS";
 export const GET_ANIMATION = "GET_ANIMATION";
 export const USERNAME = window.sessionStorage.getItem("username")
+export const IS_LOGGED_IN = window.sessionStorage.getItem("session_id") != null ? true : false

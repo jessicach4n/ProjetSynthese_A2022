0d0fa878cc794b568f3b35a1a8bdcb0a2ef97304
jessicach4n <e.jchan@etu.cvm.qc.ca>
2022-12-04 09:59:54 -0500

    show infos

--- dev/projet-synthese/src/pages/components/CardHolder.js
+++ dev/projet-synthese/src/pages/components/CardHolder.js
@@ -22,29 +22,29 @@ class CardHolder extends Component {
             this.ids = res.response;
             let cards = [];
             this.ids.forEach((id) => {   
-                cards.push(<Card key={id} username={id} video={id}/>)
-                // let formData = new FormData();
-                // formData.append("action", myConstants.GET_ANIMATION_INFO);
-                // formData.append("id", id);
-                // formData.append("session_id", window.sessionStorage.getItem("session_id"));
-                // fetch(myConstants.HOST + '/shares.php', {
-                //     method: 'POST',
-                //     body: formData
-                // })
-                // .then(res => res.json())
-                // .then(res => {
-                //     let response = res.response[0];
-                //     let video = response['id'];
-                //     let username = response['nom_utilisateur'];
-                //     let city = response['ville'];
-                //     let country = response['pays'];
-                //     let timestamp = response['date_creation'];
-                //     cards.push(<Card key={video} username={username} city={city} country={country} timestamp={timestamp} video={video}/>)
-                // })
+                // cards.push(<Card key={id} username={id} video={id}/>)
+                let formData = new FormData();
+                formData.append("action", myConstants.GET_ANIMATION_INFO);
+                formData.append("id", id);
+                formData.append("session_id", window.sessionStorage.getItem("session_id"));
+                fetch(myConstants.HOST + '/shares.php', {
+                    method: 'POST',
+                    body: formData
+                })
+                .then(res => res.json())
+                .then(res => {
+                    let response = res.response[0];
+                    let video = response['id'];
+                    let username = response['nom_utilisateur'];
+                    let city = response['ville'];
+                    let country = response['pays'];
+                    let timestamp = response['date_creation'];
+                    cards.push(<Card key={video} username={username} city={city} country={country} timestamp={timestamp} video={video}/>)
+                    this.setState({cards:cards});
+                })
             })
-            this.setState({cards:cards});
         })
-    }
+    }   
 
     render() {
         return (

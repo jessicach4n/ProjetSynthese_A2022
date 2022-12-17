e825491a26a59ea899a81df5672748550e25b124
jessicach4n <e.jchan@etu.cvm.qc.ca>
2022-12-16 20:56:25 -0500

    404 page

---
+++ dev/projet-synthese/src/pages/NotFound.js
@@ -0,0 +1,13 @@
+import '../css/index.css'
+import '../css/notfound.css'
+
+function NotFound() {
+  return (
+    <div className='oops'>
+        404
+        <div className='pas-de-panique'>La page est introuvable</div>
+    </div>
+  );
+}
+
+export default NotFound;
